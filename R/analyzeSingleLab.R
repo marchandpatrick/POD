2e9d441aff3bd3ce865638be8c82a28a22278d0b
boenn <markus.boenn.lav@gmail.com>
2019-02-25 15:41:58 +0100

    Minor release 1.00.0: see CHANGELOG for details

--- R/analyzeSingleLab.R
+++ R/analyzeSingleLab.R
@@ -7,16 +7,11 @@
 #' @param N Total number of PCR experiments.
 #' @param qLOD The quantile(s) for the Limit Of Detection (LOD). Divided by \eqn{100} if greater than one.
 #' @param b Fixed value for the corrective parameter
-#' @param sigma Fixed value for the between lab noise \ifelse{html}{\out{&sigma;<sub>L</sub>}}{\eqn{\sigma_L}}
 #' 
 #' @details 
-#' Parameter \ifelse{html}{\out{&sigma;<sub>L</sub>}}{\eqn{\sigma_L}} is required to determine the confidence interval of the lobjimit of detection (LOD) at a given quantile. It has been assessed empirically to be approximately \eqn{0.34}, leading to similar results as obtained from the original implementation. The parameter can be modified by the user.
 #' According to the suggestion of Uhlig et al. (2015), the corrective parameter \eqn{b} is set to \eqn{1} if it is close to \eqn{1} (simplified fit). However, if sensitivity is better than achievable according to the theoretical POD curve or average amplification probability is higher at higher dilution levels than at lower dilution levels, the \eqn{b} is estimated from the data (full fit). 
 #' The value of \eqn{b} can be changed by the user. However, it is not recommended to do so.
 #' 
-#' 
-#' @note A warning like \sQuote{glm.fit: fitted probabilities numerically 0 or 1 occurred} might appear during GLM computation. Can be ignored in most cases.
-#'
 #' @references
 #' Uhlig et al. Accred Qual Assur (2015) 20: 75. https://doi.org/10.1007/s00769-015-1112-9
 #' 
@@ -24,7 +19,6 @@
 #' \describe{
 #' \item{x}{Input data plus extra columns}
 #' \item{b}{The parameter \eqn{b}, as provided by the user}
-#' \item{sigma}{The parameter \ifelse{html}{\out{&sigma;<sub>L</sub>}}{\eqn{\sigma_L}}, as provided by the user}
 #' \item{fit.glm.simple}{Results for the simplified GLM}
 #' \item{fit.glm.full}{Results for the full GLM}
 #' }
@@ -50,34 +44,43 @@
 #' )
 #' obj <- analyzeSingleLab(x=x)
 
-analyzeSingleLab <- function(x=NULL, X=NULL, S=NULL, N=NULL, qLOD=95, b=1, sigma=0.35){
+analyzeSingleLab <- function(x=NULL, X=NULL, S=NULL, N=NULL, qLOD=95, b=1){
     obj <- list()
     .COLNAMES <- c('X', 'S', 'N')
     .b <- b
     if(qLOD[1] > 1){ qLOD <- qLOD/100 } # force quantiles to [0,1]
     qLOD <- sort(qLOD)
     
-    # compute LOD and confidence intervals
-    .getlod <- function(lambda, b){
-      vLOD <- ( -log(1-qLOD) / lambda )**(1/b)
-      names(vLOD) <- as.character(qLOD*100)
-      LODLL <- 1/b * ( log(-log(1-qLOD)) - (log(lambda)+1.96*sigma) ); LODLL <- exp(LODLL)
-      LODUL <- 1/b * ( log(-log(1-qLOD)) - (log(lambda)-1.96*sigma) ); LODUL <- exp(LODUL)
-      out <- do.call("cbind", list(vLOD, LODLL, LODUL))
-      colnames(out) <- c("LOD", "UL", "LL")
-    return( out )
-    }
-    
     # compute curve 95% confidence interval (cip95)
-    .getcip95 <- function(GLM, b){
+    .getcip95 <- function(GLM, b, v=NULL){
       ilink <- family(GLM)$linkinv
       fit <- se.fit <- NULL
-      pd <- data.frame(X = sort(c(seq(min(x$X), max(x$X), length = 100), seq(0.5, 1.5, 0.01))), b=b)
+      aux <- v
+      if(is.null(v)){ aux <- c(seq(min(x$X), max(x$X), length = 100), seq(0.5, 1.5, 0.01)) }
+      pd <- data.frame(X = sort(aux), b=b)
       pd <- cbind(pd, predict(GLM, pd, type = 'link', se.fit = TRUE)[1:2])
       pd <- transform(pd, Fitted = ilink(fit), Upper = ilink(fit + (1.96 * se.fit)), Lower = ilink(fit - (1.96 * se.fit)))
     return(pd[, c("X", "Upper", "Lower")])
     }
-    
+
+    # compute LOD and confidence intervals
+     .getlod <- function(lambda, b, cip95, mod){
+        vLOD <- ( -log(1-qLOD) / lambda )**(1/b)
+        names(vLOD) <- as.character(qLOD*100)
+        LODLL <- LODUL <- rep(0, length(vLOD))
+        for( iq in seq(qLOD) ){
+            up <- cip95[which.min( abs(cip95$Upper-qLOD[iq]) )+c(-1,0,1), 1]
+            lo <- cip95[which.min( abs(cip95$Lower-qLOD[iq]) )+c(-1,0,1), 1]
+            cip95up <- .getcip95(mod, b, seq(min(up), max(up), 0.001))
+            cip95lo <- .getcip95(mod, b, seq(min(lo), max(lo), 0.001))
+            LODUL[iq] <- cip95up[which.min( abs(cip95up$Upper-qLOD[iq]) ), 1]
+            LODLL[iq] <- cip95lo[which.min( abs(cip95lo$Lower-qLOD[iq]) ), 1]
+        }
+        out <- do.call("cbind", list(vLOD, LODLL, LODUL))
+        colnames(out) <- c("LOD", "LL", "UL")
+    return( out )
+    }
+
     .catchWarn <- function(w){WARN <<- append(WARN, conditionMessage(w)); invokeRestart("muffleWarning");}
 
     if( is.null(x) ){
@@ -118,7 +121,8 @@ analyzeSingleLab <- function(x=NULL, X=NULL, S=NULL, N=NULL, qLOD=95, b=1, sigma
     GLMSIMPLE <- withCallingHandlers( glm( Y ~ offset(b*log(X)), data=x, family=binomial(link="cloglog"), weights=N ), warning=function(w){.catchWarn(w)} )
     sGLMSIMPLE <- summary(GLMSIMPLE)$coefficients
     lambda <- exp(sGLMSIMPLE[1,1])
-    FIT.SIMPLE <- list( b=.b, lambda=lambda, model=GLMSIMPLE, LOD=.getlod(lambda, b), cip95=.getcip95(GLMSIMPLE, b), warn=WARN )
+    cip95 <- .getcip95(GLMSIMPLE, b)
+    FIT.SIMPLE <- list( b=.b, lambda=lambda, model=GLMSIMPLE, LOD=.getlod(lambda, b, cip95, GLMSIMPLE), cip95=cip95, warn=WARN )
     if( length(setdiff( WARN, expWarnings )) ){ warning(WARN) }
     
     ## full fit
@@ -127,12 +131,12 @@ analyzeSingleLab <- function(x=NULL, X=NULL, S=NULL, N=NULL, qLOD=95, b=1, sigma
     sGLMFULL <- summary(GLMFULL)$coefficients
     lambda <- exp(sGLMFULL[1,1])
     b <- sGLMFULL[2,1]
-    FIT.FULL <- list( b=b, lambda=lambda, model=GLMFULL, LOD=.getlod(lambda, b), cip95=.getcip95(GLMFULL, b), warn=WARN )
+    cip95 <- .getcip95(GLMFULL, b)
+    FIT.FULL <- list( b=b, lambda=lambda, model=GLMFULL, LOD=.getlod(lambda, b, cip95, GLMFULL), cip95=cip95, warn=WARN )
     if( length(setdiff( WARN, expWarnings )) ){ warning(WARN) }
     
     obj[["x"]] <- x
     obj[["b"]] <- .b
-    obj[["sigma"]] <- sigma
     obj[["fit.glm.simple"]] <- FIT.SIMPLE
     obj[["fit.glm.full"]] <- FIT.FULL
     class(obj) <- "pod"
